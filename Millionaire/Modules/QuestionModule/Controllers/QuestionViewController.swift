d8712c556e40dc73f6524af83cac033395c07799
koko6a <rus.baranovskiy@gmail.com>
2023-02-07 22:16:44 +0300

    feature/createModels

--- Millionaire/Modules/QuestionModule/Controllers/QuestionViewController.swift
+++ Millionaire/Modules/QuestionModule/Controllers/QuestionViewController.swift
@@ -1,10 +1,13 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+    
+    let questionManager = QuestionManager(totalQuestions: 15)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print(questionManager.testArrays())
         
     }
 }
