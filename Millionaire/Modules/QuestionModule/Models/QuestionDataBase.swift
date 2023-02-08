53a2e83841c9304b9df1633af42026697b77b9fe
koko6a <rus.baranovskiy@gmail.com>
2023-02-08 04:35:10 +0300

    Add new func to fetch random question / fixed

--- Millionaire/Modules/QuestionModule/Models/QuestionDataBase.swift
+++ Millionaire/Modules/QuestionModule/Models/QuestionDataBase.swift
@@ -9,84 +9,144 @@ struct QuestionDataBase {
         Question(
             question: "LOW 1",
             answers: Answer(
-                aAnswer: [false : "4"], bAnswer: [false : "15"],
+                aAnswer: [false : "4"], bAnswer: [false : "5"],
                 cAnswer: [true : "1"], dAnswer: [false : "3"])),
         Question(
             question: "LOW 2",
             answers: Answer(
-                aAnswer: [true : "2"], bAnswer: [false : "4"],
-                cAnswer: [false : "15"], dAnswer: [false : "30"])),
+                aAnswer: [true : "2"], bAnswer: [false : "1"],
+                cAnswer: [false : "4"], dAnswer: [false : "3"])),
         Question(
             question: "LOW 3",
             answers: Answer(
-                aAnswer: [false : "313"], bAnswer: [false : "91"],
-                cAnswer: [false : "1"], dAnswer: [true : "3"])),
+                aAnswer: [false : "4"], bAnswer: [false : "1"],
+                cAnswer: [false : "2"], dAnswer: [true : "3"])),
         Question(
             question: "LOW 4",
             answers: Answer(
-                aAnswer: [false : "2"], bAnswer: [false : "18"],
-                cAnswer: [true : "4"], dAnswer: [false : "1337"])),
+                aAnswer: [false : "2"], bAnswer: [false : "3"],
+                cAnswer: [true : "4"], dAnswer: [false : "1"])),
         Question(
             question: "LOW 5",
             answers: Answer(
-                aAnswer: [true : "90"], bAnswer: [true : "5"],
-                cAnswer: [false : "4"], dAnswer: [false : "55"]))
+                aAnswer: [true : "1"], bAnswer: [true : "5"],
+                cAnswer: [false : "2"], dAnswer: [false : "3"])),
+        Question(
+            question: "LOW 6",
+            answers: Answer(
+                aAnswer: [false : "8"], bAnswer: [false : "7"],
+                cAnswer: [true : "6"], dAnswer: [false : "9"])),
+        Question(
+            question: "LOW 7",
+            answers: Answer(
+                aAnswer: [true : "7"], bAnswer: [false : "8"],
+                cAnswer: [false : "6"], dAnswer: [false : "9"])),
+        Question(
+            question: "LOW 8",
+            answers: Answer(
+                aAnswer: [false : "9"], bAnswer: [false : "7"],
+                cAnswer: [false : "6"], dAnswer: [true : "8"])),
+        Question(
+            question: "LOW 9",
+            answers: Answer(
+                aAnswer: [false : "8"], bAnswer: [false : "6"],
+                cAnswer: [true : "9"], dAnswer: [false : "7"])),
     ]
     
     private let mediumQuestions: [Question] = [
         Question(
             question: "MEDIUM 1",
             answers: Answer(
-                aAnswer: [false : "4"], bAnswer: [false : "15"],
+                aAnswer: [false : "4"], bAnswer: [false : "5"],
                 cAnswer: [true : "1"], dAnswer: [false : "3"])),
         Question(
             question: "MEDIUM 2",
             answers: Answer(
-                aAnswer: [true : "2"], bAnswer: [false : "4"],
-                cAnswer: [false : "15"], dAnswer: [false : "30"])),
+                aAnswer: [true : "2"], bAnswer: [false : "1"],
+                cAnswer: [false : "4"], dAnswer: [false : "3"])),
         Question(
             question: "MEDIUM 3",
             answers: Answer(
-                aAnswer: [false : "313"], bAnswer: [false : "91"],
-                cAnswer: [false : "1"], dAnswer: [true : "3"])),
+                aAnswer: [false : "4"], bAnswer: [false : "1"],
+                cAnswer: [false : "2"], dAnswer: [true : "3"])),
         Question(
             question: "MEDIUM 4",
             answers: Answer(
-                aAnswer: [false : "2"], bAnswer: [false : "18"],
-                cAnswer: [true : "4"], dAnswer: [false : "1337"])),
+                aAnswer: [false : "2"], bAnswer: [false : "3"],
+                cAnswer: [true : "4"], dAnswer: [false : "1"])),
         Question(
             question: "MEDIUM 5",
             answers: Answer(
-                aAnswer: [true : "90"], bAnswer: [true : "5"],
-                cAnswer: [false : "4"], dAnswer: [false : "55"]))
+                aAnswer: [true : "1"], bAnswer: [true : "5"],
+                cAnswer: [false : "2"], dAnswer: [false : "3"])),
+        Question(
+            question: "MEDIUM 6",
+            answers: Answer(
+                aAnswer: [false : "8"], bAnswer: [false : "7"],
+                cAnswer: [true : "6"], dAnswer: [false : "9"])),
+        Question(
+            question: "MEDIUM 7",
+            answers: Answer(
+                aAnswer: [true : "7"], bAnswer: [false : "8"],
+                cAnswer: [false : "6"], dAnswer: [false : "9"])),
+        Question(
+            question: "MEDIUM 8",
+            answers: Answer(
+                aAnswer: [false : "9"], bAnswer: [false : "7"],
+                cAnswer: [false : "6"], dAnswer: [true : "8"])),
+        Question(
+            question: "MEDIUM 9",
+            answers: Answer(
+                aAnswer: [false : "8"], bAnswer: [false : "6"],
+                cAnswer: [true : "9"], dAnswer: [false : "7"])),
     ]
     
     private let hardQuestions: [Question] = [
         Question(
-            question: "HARD Выбери число 1",
+            question: "HARD 1",
             answers: Answer(
-                aAnswer: [false : "4"], bAnswer: [false : "15"],
+                aAnswer: [false : "4"], bAnswer: [false : "5"],
                 cAnswer: [true : "1"], dAnswer: [false : "3"])),
         Question(
-            question: "HARD Выбери число 2",
+            question: "HARD 2",
             answers: Answer(
-                aAnswer: [true : "2"], bAnswer: [false : "4"],
-                cAnswer: [false : "15"], dAnswer: [false : "30"])),
+                aAnswer: [true : "2"], bAnswer: [false : "1"],
+                cAnswer: [false : "4"], dAnswer: [false : "3"])),
         Question(
-            question: "HARD Выбери число 3",
+            question: "HARD 3",
             answers: Answer(
-                aAnswer: [false : "313"], bAnswer: [false : "91"],
-                cAnswer: [false : "1"], dAnswer: [true : "3"])),
+                aAnswer: [false : "4"], bAnswer: [false : "1"],
+                cAnswer: [false : "2"], dAnswer: [true : "3"])),
         Question(
-            question: "HARD Выбери число 4",
+            question: "HARD 4",
             answers: Answer(
-                aAnswer: [false : "2"], bAnswer: [false : "18"],
-                cAnswer: [true : "4"], dAnswer: [false : "1337"])),
+                aAnswer: [false : "2"], bAnswer: [false : "3"],
+                cAnswer: [true : "4"], dAnswer: [false : "1"])),
         Question(
-            question: "HARD Выбери число 5",
+            question: "HARD 5",
             answers: Answer(
-                aAnswer: [true : "90"], bAnswer: [true : "5"],
-                cAnswer: [false : "4"], dAnswer: [false : "55"]))
+                aAnswer: [true : "1"], bAnswer: [true : "5"],
+                cAnswer: [false : "2"], dAnswer: [false : "3"])),
+        Question(
+            question: "HARD 6",
+            answers: Answer(
+                aAnswer: [false : "8"], bAnswer: [false : "7"],
+                cAnswer: [true : "6"], dAnswer: [false : "9"])),
+        Question(
+            question: "HARD 7",
+            answers: Answer(
+                aAnswer: [true : "7"], bAnswer: [false : "8"],
+                cAnswer: [false : "6"], dAnswer: [false : "9"])),
+        Question(
+            question: "HARD 8",
+            answers: Answer(
+                aAnswer: [false : "9"], bAnswer: [false : "7"],
+                cAnswer: [false : "6"], dAnswer: [true : "8"])),
+        Question(
+            question: "HARD 9",
+            answers: Answer(
+                aAnswer: [false : "8"], bAnswer: [false : "6"],
+                cAnswer: [true : "9"], dAnswer: [false : "7"])),
     ]
     
     func fetchRandomLowQuestions() -> [Question] {
--- Millionaire/Modules/QuestionModule/Models/QuestionManager.swift
+++ Millionaire/Modules/QuestionModule/Models/QuestionManager.swift
@@ -3,11 +3,10 @@ import Foundation
 /// Data Manager to fetch any questions from QuestionDataBase
 final class QuestionManager {
     
-    let totalQuestions: Int
+    static let shared = QuestionManager()
+    private (set) var currentQuestion: Question?
     
-    init(totalQuestions: Int) {
-        self.totalQuestions = totalQuestions
-    }
+    private let totalQuestions = 15
     
     private let lowQuestions = QuestionDataBase.shared.fetchRandomLowQuestions()
     private let mediumQuestions = QuestionDataBase.shared.fetchRandomMediumQuestions()
@@ -15,19 +14,29 @@ final class QuestionManager {
     
     private var currentQuestionIndex = 0
     private var currentTypeQuestion: QuestionType = .low
-    
-    private var currentQuestion: Question?
-    
+        
     // Func to fetch new request for different levels
     func fetchNewQuestion() -> Question? {
+        updateCurrentQuestion()
+        return currentQuestion
+    }
+    
+    // Func to check on the right answer
+    func checkAnswer(with answer: [Bool: String]) -> Bool {
+
+        return false
+    }
+    
+    // Private func to generate random quesion
+    private func updateCurrentQuestion() {
         let totalForEach = totalQuestions / 3
         
         if totalQuestions % 3 != 0 {
-            return nil
+            return
         }
         
         if lowQuestions.count < totalForEach || mediumQuestions.count < totalForEach || hardQuestions.count < totalForEach {
-            return nil
+            return
         }
         
         switch currentTypeQuestion {
@@ -57,6 +66,5 @@ final class QuestionManager {
                 currentQuestionIndex += 1
             }
         }
-        return nil
     }
 }
