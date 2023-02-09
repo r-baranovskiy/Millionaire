24ed1c70fbff0231d1e2543fde705a2a75b2546f
Yan Shvyndikov <Yansv2006@gmail.com>
2023-02-09 23:18:14 +0300

    WinLose

---
+++ Millionaire/Modules/ResultModule/Controllers/LoseViewController.swift
@@ -0,0 +1,74 @@
+
+import UIKit
+
+final class LoseViewController: UIViewController {
+    
+    private let gradientlayer = UIImageView(image: UIImage(named: "backgroundBlue")!, contentMode: .scaleAspectFit)
+    private let mainLogo = UIImageView(image: UIImage(named: "logoLarge")!, contentMode: .scaleAspectFit)
+    
+    private let gameStopped = UILabel(text: "ИГРА ОКОНЧЕНА", font: .systemFont(ofSize: 28))
+    private let yourWinnings = UILabel(text: "Ваш выигрыш: 0₽", font: .systemFont(ofSize: 22))
+    
+    private let loseImageView = UIImageView(image: UIImage(named: "loseResultIcon")!, contentMode: .scaleAspectFit)
+    
+    private let playAgainButton = CustomButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        playAgainButton.setTitle("играть еще раз", for: .normal)
+        
+        playAgainButton.addTarget(self, action: #selector(playAgainButtonPressed), for: .touchUpInside)
+        
+        setConstraints()
+    }
+    
+    @objc
+    private func playAgainButtonPressed() {
+        // code
+    }
+}
+
+// MARK: - LoseViewControllerConstraints
+extension LoseViewController {
+    private func setConstraints() {
+        self.view.addSubview(gradientlayer)
+        self.view.addSubview(mainLogo)
+        self.view.addSubview(gameStopped)
+        self.view.addSubview(yourWinnings)
+        self.view.addSubview(loseImageView)
+        self.view.addSubview(playAgainButton)
+        
+        NSLayoutConstraint.activate([
+            gradientlayer.topAnchor.constraint(equalTo: view.topAnchor),
+            gradientlayer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            gradientlayer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            gradientlayer.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            mainLogo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainLogo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainLogo.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
+            mainLogo.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3),
+            
+            gameStopped.topAnchor.constraint(equalTo: mainLogo.bottomAnchor, constant: 40),
+            gameStopped.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            gameStopped.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            gameStopped.heightAnchor.constraint(equalToConstant: 30),
+            
+            yourWinnings.topAnchor.constraint(equalTo: gameStopped.bottomAnchor, constant: 10),
+            yourWinnings.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            yourWinnings.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            yourWinnings.heightAnchor.constraint(equalToConstant: 30),
+            
+            loseImageView.topAnchor.constraint(equalTo: yourWinnings.bottomAnchor, constant: 50),
+            loseImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 80),
+            loseImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -80),
+            loseImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -220),
+            
+            playAgainButton.topAnchor.constraint(equalTo: loseImageView.bottomAnchor, constant: 20),
+            playAgainButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60),
+            playAgainButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -60),
+            playAgainButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+}
--- Millionaire/Modules/RulesModule/Controllers/RulesViewController.swift
+++ Millionaire/Modules/RulesModule/Controllers/RulesViewController.swift
@@ -7,20 +7,59 @@ final class RulesViewController: UIViewController {
     
     private let rulesLabel = UILabel(text: "Правила игры", font: .systemFont(ofSize: 42))
     
+    private let custView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .bottomColor()
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let rulesTextVeiw: UITextView = {
+        let textView = UITextView()
+        textView.text = "Задача организации, в особенности же консультация с широким активом обеспечивает широкому кругу (специалистов) участие в формировании модели развития. Равным образом укрепление и развитие структуры позволяет оценить значение форм развития. Значимость этих проблем настолько очевидна, что реализация намеченных плановых заданий способствует подготовки и реализации форм развития. С другой стороны начало повседневной работы по формированию позиции в значительной степени обуславливает создание системы обучения кадров, соответствует насущным потребностям. "
+        textView.backgroundColor = .clear
+        textView.textColor = .white
+        textView.font = .systemFont(ofSize: 18)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    private let backButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Назад", for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .clear
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .black
         
+        backButton.addTarget(self, action: #selector(backButtonPressed), for: .touchUpInside)
+        
         setConstraints()
+    }
+    
+    @objc
+    private func backButtonPressed() {
+        let vc = StartViewController()
+        navigationController?.pushViewController(vc, animated: false)
     }
 }
 
+// MARK: - RulesViewControllerConstraints
 extension RulesViewController {
     private func setConstraints() {
         self.view.addSubview(gradientlayer)
         self.view.addSubview(mainLogo)
         self.view.addSubview(rulesLabel)
+        self.view.addSubview(custView)
+        custView.addSubview(rulesTextVeiw)
+        self.view.addSubview(backButton)
         
         NSLayoutConstraint.activate([
             gradientlayer.topAnchor.constraint(equalTo: view.topAnchor),
@@ -32,11 +71,26 @@ extension RulesViewController {
             mainLogo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             mainLogo.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
             mainLogo.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2),
-    
+            
             rulesLabel.topAnchor.constraint(equalTo: mainLogo.bottomAnchor, constant: 20),
             rulesLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             rulesLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             rulesLabel.heightAnchor.constraint(equalToConstant: 40),
+            
+            custView.topAnchor.constraint(equalTo: rulesLabel.bottomAnchor, constant: 20),
+            custView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            custView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            custView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -70),
+            
+            rulesTextVeiw.topAnchor.constraint(equalTo: custView.topAnchor, constant: 25),
+            rulesTextVeiw.leadingAnchor.constraint(equalTo: custView.leadingAnchor, constant: 16),
+            rulesTextVeiw.trailingAnchor.constraint(equalTo: custView.trailingAnchor, constant: -16),
+            rulesTextVeiw.bottomAnchor.constraint(equalTo: custView.bottomAnchor, constant: -40),
+            
+            backButton.topAnchor.constraint(equalTo: custView.bottomAnchor, constant: 5),
+            backButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            backButton.widthAnchor.constraint(equalToConstant: 70),
+            backButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30)
         ])
     }
 }
