25e97d6e03103dcb41924c3dde9e00c7e14ad16a
koko6a <rus.baranovskiy@gmail.com>
2023-02-10 14:49:28 +0300

    Write fix and tasks for StartModule

---
+++ Millionaire/BugFix/Tasks.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+// MARK: - RulesModule
+
+//1. Write the rules
+//2. Set navigation for the transition to the game module
---
+++ Millionaire/BugFix/UI Bugs.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+// MARK: - StartModule
+
+/*
+//Fix size for small screen
+ 
+private let gradientlayer = UIImageView(image: UIImage(named: "backgroundBlue")!, contentMode: .scaleAspectFill)
+
+*/
+
+// MARK: - Extension UILabelST
+
+//Color for dark theme doesn't work (Fix)
+/*
+extension UILabel {
+    convenience init(text: String, font: UIFont){
+        self.init()
+
+        self.text = text
+        self.font = font
+        self.textColor = .white
+        self.textAlignment = .center
+        self.numberOfLines = 0
+        self.adjustsFontSizeToFitWidth = true
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+*/
